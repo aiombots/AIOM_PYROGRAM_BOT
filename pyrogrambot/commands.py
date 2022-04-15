1db70be6b72527e851923d22c8e205fd5b449a91
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-15 22:38:14 +0530

    Update commands.py

--- pyrogrambot/commands.py
+++ pyrogrambot/commands.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogrambot.photos import PHOTOS
 from pyrogrambot.buttons import button
 from pyrogram.types import InlineKeyboardMarkup
+import random
 
 @Client.on_message(filters.command("start")) 
 async def start_message(bot, message):
