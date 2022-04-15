3b5777c42cdf919fa57df3590008a9c484b0b53f
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-16 05:07:30 +0530

    Update buttons.py

--- pyrogrambot/buttons.py
+++ pyrogrambot/buttons.py
@@ -3,3 +3,8 @@ from pyrogram.types import InlineKeyboardButton
 button = [[
          InlineKeyboardButton("Mᴇɴᴜ", callback_data="next")
          ]]
+
+MENU_BUTTON = [[
+              InlineKeyboardButton("Sᴛɪᴄᴋᴇʀ", callback_data="sticker"),
+              InlineKeyboardButton("Vɪᴅᴇᴏ", callback_data="video")
+              ]]
