5c119570e692cfe76d0f9cca2bc68a3cd58d98d6
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-15 23:20:56 +0530

    Update commands.py

--- pyrogrambot/commands.py
+++ pyrogrambot/commands.py
@@ -4,10 +4,13 @@ from pyrogrambot.buttons import button
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 import random
+import asyncio
 FORCE_SUB = "AIOM_PYRO"
 
 @Client.on_message(filters.command("start")) 
 async def start_message(bot, message):
+    await bot.send_chat_action(chat_id=message.from_user.id, "Typing")
+    await asyncio.sleep(2)
     if FORCE_SUB:
         try:
             user = await bot.get_chat_member(FORCE_SUB, message.chat.id)
