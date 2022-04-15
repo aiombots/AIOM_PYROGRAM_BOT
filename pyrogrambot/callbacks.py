a6f470302086e4af4c7eb36721779c3aab708cf2
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-16 05:16:08 +0530

    Update callbacks.py

--- pyrogrambot/callbacks.py
+++ pyrogrambot/callbacks.py
@@ -1,15 +1,33 @@
 from pyrogram.types import CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup
 from pyrogram import Client
+from pyrogrambot.photos import PHOTOS
+import random
+from pyrogrambot.buttons import MENU_BUTTON
+import asyncio
+
 
 @Client.on_callback_query()
 async def callback(bot, msg: CallbackQuery):
     if msg.data == "next":
         await msg.message.edit("○○○○○")
+        asyncio.sleep(0.9)
         await msg.message.edit("●○○○○")
+        asyncio.sleep(0.9)
         await msg.message.edit("●●○○○")
+        asyncio.sleep(0.9)
         await msg.message.edit("●●●○○")
+        asyncio.sleep(0.9)
         await msg.message.edit("●●●●○")
+        asyncio.sleep(0.9)
         await msg.message.edit("●●●●●")
+        asyncio.sleep(0.9)
         await msg.message.edit(
-            text=f"hi"
+            photo=random.choice(PHOTOS),
+            reply_markup=InlineKeyboardMarkup(MENU_BUTTON)
+        )
+    elif msg.data == "sticker":
+        await msg.message.edit(
+            sticker="CAACAgIAAxkBAAECR5FiWgOUsaX2iRWuUtv8Y7AvIPoNuQAC-hAAAqHHKEg5ZXbrk1gHox4E",
+            reply_markup=InlineKeyboardMarkup(MENU_BUTTON)
         )
