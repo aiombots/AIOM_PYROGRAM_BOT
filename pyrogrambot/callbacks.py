1c9e22dd2356ffbcd7292de12a086ff736435b0e
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-16 05:19:42 +0530

    Update callbacks.py

--- pyrogrambot/callbacks.py
+++ pyrogrambot/callbacks.py
@@ -1,8 +1,6 @@
 from pyrogram.types import CallbackQuery
 from pyrogram.types import InlineKeyboardMarkup
 from pyrogram import Client
-from pyrogrambot.photos import PHOTOS
-import random
 from pyrogrambot.buttons import MENU_BUTTON
 import asyncio
 
@@ -23,7 +21,6 @@ async def callback(bot, msg: CallbackQuery):
         await msg.message.edit("●●●●●")
         asyncio.sleep(0.9)
         await msg.message.edit(
-            photo=random.choice(PHOTOS),
             reply_markup=InlineKeyboardMarkup(MENU_BUTTON)
         )
     elif msg.data == "sticker":
