09de8919098321087397b983f561ad778413f933
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-15 23:53:29 +0530

    Update callbacks.py

--- pyrogrambot/callbacks.py
+++ pyrogrambot/callbacks.py
@@ -1,1 +1,15 @@
+from pyrogram.types import CallbackQuery
+from pyrogram import Client
 
+@Client.on_callback_query()
+async def callback(bot, msg: CallbackQuery):
+    if msg.data == "next":
+        await msg.message.edit("○○○○○")
+        await msg.message.edit("●○○○○")
+        await msg.message.edit("●●○○○")
+        await msg.message.edit("●●●○○")
+        await msg.message.edit("●●●●○")
+        await msg.message.edit("●●●●●")
+        await msg.message.edit(
+            text=f" hello {msg.from_user.mention}  Start Text"
+        )
