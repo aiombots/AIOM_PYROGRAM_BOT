236aeea4221c95eaa2682f693cd31e482232041a
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-15 22:51:03 +0530

    Update photos.py

--- pyrogrambot/photos.py
+++ pyrogrambot/photos.py
@@ -1,5 +1,9 @@
 
 PHOTOS = [
-         "https://telegra.ph/file/517bdca1f232bc8f7de28.jpg",
-         "https://telegra.ph/file/5025089cc3a6ee4a3b15e.jpg"
+         "https://telegra.ph/file/ad57acb6a695288baef12.jpg",
+         "https://telegra.ph/file/eaa658c12d3c58834a4cb.jpg",
+         "https://telegra.ph/file/98c3e37e883a7438a4ccb.jpg",
+         "https://telegra.ph/file/7fe0b9b6d57c2efd04733.jpg",
+         "https://telegra.ph/file/6aef82152c52e15a07ffb.jpg",
+         "https://telegra.ph/file/73243b0528ed9a2bd526b.jpg"
          ]
