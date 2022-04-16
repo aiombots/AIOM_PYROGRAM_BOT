9f3cc0d1145004cf12baf3a898ba5af10ca30d66
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-16 07:07:57 +0530

    Update filters.py

--- pyrogrambot/filters.py
+++ pyrogrambot/filters.py
@@ -1,1 +1,12 @@
+from pyrogram import Client, filters
+from pyrogrambot.photos import KGF_PHOTOS
+from pyrogrambot.buttons import KGF_D_BUTTON
+import random
+
+@Client.on_message(filters.regex("kgf 2") & filters.private)
+async def kgf_filter(bot, message):
+    await message.reply_photo(
+        photo=random.choice(KGF_PHOTOS),
+        reply_markup=InlineKeyboardMarkup(KGF_D_BUTTON)
+    )
 
