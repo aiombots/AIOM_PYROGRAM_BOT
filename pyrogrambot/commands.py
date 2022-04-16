8e89f13f56af2bcd82845c7d189dc58625214ff2
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-16 06:51:41 +0530

    Update commands.py

--- pyrogrambot/commands.py
+++ pyrogrambot/commands.py
@@ -50,5 +50,6 @@ Cʟɪᴄᴋ Bᴇʟᴏᴡ Bᴜᴛᴛᴏɴ Tᴏ Sᴇᴇ Mᴏʀᴇ</b>""",
 async def id_message(bot, msg):
     text = f"""Tɪᴛʟᴇ : {msg.chat.title}
 Usᴇʀɴᴀᴍᴇ : @{msg.chat.username}
-Cʜᴀᴛ ɪᴅ : {msg.chat.id}"""
+Cʜᴀᴛ ɪᴅ : `{msg.chat.id}`
+Usᴇʀ ɪᴅ : `{msg.from_user.id}`"""
     await msg.reply_text(text=text)
