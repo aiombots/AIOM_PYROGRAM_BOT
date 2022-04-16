3a873320cc6224b6a69e54ba3a38176b90a63990
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-16 07:29:49 +0530

    Update buttons.py

--- pyrogrambot/buttons.py
+++ pyrogrambot/buttons.py
@@ -31,3 +31,14 @@ COMMM_BUTTON = [[
 KGF_D_BUTTON = [[
                InlineKeyboardButton("D O W N L O A D", callback_data="downlod")
                ]]
+
+KGF_BUTTON = [[
+             InlineKeyboardButton("200MB", url="https://t.me/AIOM_PYRO/7"),
+             InlineKeyboardButton("400MB", url="https://t.me/AIOM_PYRO/7"),
+             InlineKeyboardButton("700MB", url="https://t.me/AIOM_PYRO/7")
+             ],[
+             InlineKeyboardButton("1.2GB", url="https://t.me/AIOM_PYRO/7"),
+             InlineKeyboardButton("1.7GB", url="https://t.me/AIOM_PYRO/7"),
+             InlineKeyboardButton("Cʟᴏꜱᴇ", callback_data="close")
+             ]]
+
